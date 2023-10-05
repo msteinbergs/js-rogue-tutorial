0b832c1b088c44ddd3fed722071879fbbb13e535
Matt Steinbergs <msteinbergs@proton.me>
2023-10-04 23:06:24 -0500

    adds input-handler

---
+++ src/input-handler.ts
@@ -0,0 +1,26 @@
+export interface Action {}
+
+export class MovementAction implements Action {
+  dx: number;
+  dy: number;
+
+  constructor(dx: number, dy: number) {
+    this.dx = dx;
+    this.dy = dy;
+  }
+}
+
+interface MovementMap {
+  [key: string]: Action;
+}
+
+const MOVE_KEYS: MovementMap = {
+  ArrowUp: new MovementAction(0, 1),
+  ArrowDown: new MovementAction(0, -1),
+  ArrowLeft: new MovementAction(-1, 0),
+  ArrowRight: new MovementAction(1, 0),
+};
+
+export function handleInput(event: KeyboardEvent): Action {
+  return MOVE_KEYS[event.key];
+}
